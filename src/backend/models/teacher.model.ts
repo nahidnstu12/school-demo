312635d12b3eaa899912d928d32da75f76c97e5a
nahidnstu12 <nahid.dev19@gmail.com>
2025-04-30 13:30:30 +0600

    refactor(ui): vercel deployment config

--- src/backend/models/teacher.model.ts
+++ src/backend/models/teacher.model.ts
@@ -8,16 +8,6 @@ class TeacherModel extends BaseModel<Teacher> {
   constructor() {
     super(prisma.teacher);
   }
-
-  // async findUnique(id: string | number): Promise<Teacher | null> {
-  //   return await this.model.findUnique({
-  //     where: { id },
-  //     include: {
-  //       user: true,
-  //       institution: true,
-  //     },
-  //   });
-  // }
 }
 
 export default TeacherModel;
